fc899727e4467eb3696b91acd51b37ae45397247
Steven <steven@corecc.net>
2014-03-19 16:46:46 -0400

    Organization Added

---
+++ EsportsFundraising/Controllers/ESFOrganizationController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EsportsFundraising.Models;
+
+namespace EsportsFundraising.Controllers
+{
+    public class ESFOrganizationController : Controller
+    {
+        private ESFDB db = new ESFDB();
+
+        // GET: /ESFOrganization/
+        public ActionResult Index()
+        {
+            return View(db.Organizations.ToList());
+        }
+
+        // GET: /ESFOrganization/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Organization organization = db.Organizations.Find(id);
+            if (organization == null)
+            {
+                return HttpNotFound();
+            }
+            return View(organization);
+        }
+
+        // GET: /ESFOrganization/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /ESFOrganization/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="OrganizationID,Name,State,PostalCode,Members,SalesRepID,Active")] Organization organization)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Organizations.Add(organization);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(organization);
+        }
+
+        // GET: /ESFOrganization/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Organization organization = db.Organizations.Find(id);
+            if (organization == null)
+            {
+                return HttpNotFound();
+            }
+            return View(organization);
+        }
+
+        // POST: /ESFOrganization/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="OrganizationID,Name,State,PostalCode,Members,SalesRepID,Active")] Organization organization)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(organization).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(organization);
+        }
+
+        // GET: /ESFOrganization/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Organization organization = db.Organizations.Find(id);
+            if (organization == null)
+            {
+                return HttpNotFound();
+            }
+            return View(organization);
+        }
+
+        // POST: /ESFOrganization/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Organization organization = db.Organizations.Find(id);
+            db.Organizations.Remove(organization);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EsportsFundraising/Models/Organization.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace EsportsFundraising.Models
+{
+    public class Organization
+    {
+        private Int32 _organizationID = 0;
+        private string _name = "";
+        private string _state = "";
+        private string _postalCode = "";
+        private Int32 _members = 0;
+        private Int32 _salesRepID = 0;
+        private Int32 _active = 0;
+
+        public Int32 OrganizationID
+        {
+            get
+            {
+                return _organizationID;
+            }
+            set
+            {
+                _organizationID = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public string State
+        {
+            get
+            {
+                return _state;
+            }
+            set
+            {
+                _state = value;
+            }
+        }
+
+        public string PostalCode
+        {
+            get
+            {
+                return _postalCode;
+            }
+            set
+            {
+                _postalCode = value;
+            }
+        }
+
+        public Int32 Members
+        {
+            get
+            {
+                return _members;
+            }
+            set
+            {
+                _members = value;
+            }
+        }
+
+        public Int32 SalesRepID
+        {
+            get
+            {
+                return _salesRepID;
+            }
+            set
+            {
+                _salesRepID = value;
+            }
+        }
+
+        public Int32 Active
+        {
+            get
+            {
+                return _active;
+            }
+            set
+            {
+                _active = value;
+            }
+        }
+    }
+    public class ESFDB : DbContext
+    {
+        public DbSet<Organization> Organizations {get;set;}
+    }
+}
